f5b26b225d6dcdb523acdc6c9fa13ff6b4bbfa16
IwayRiway <riway.restu@gmail.com>
2021-01-25 11:01:17 +0700

    first commit

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,47 @@
+<!-- header area start -->
+<div class="header-area">
+   <div class="row align-items-center">
+       <!-- nav and search button -->
+       <div class="col-md-6 col-sm-8 clearfix">
+           <div class="nav-btn pull-left">
+               <span></span>
+               <span></span>
+               <span></span>
+           </div>
+           <div class="search-box pull-left">
+           </div>
+       </div>
+       <!-- profile info & task notification -->
+       <div class="col-md-6 col-sm-4 clearfix">
+           <ul class="notification-area pull-right">
+               <li id="full-view"><i class="ti-fullscreen"></i></li>
+               <li id="full-view-exit"><i class="ti-zoom-out"></i></li>
+               <li class="dropdown">
+                   <i class="fa fa-envelope-o dropdown-toggle" data-toggle="dropdown"><span>3</span></i>
+                   <div class="dropdown-menu notify-box nt-enveloper-box">
+                       <span class="notify-title">You have 3 new notifications <a href="#">view all</a></span>
+                       <div class="nofity-list">
+                           <a href="#" class="notify-item">
+                               <div class="notify-thumb">
+                                   <img src="{{asset('assets/images/author/author-img1.jpg')}}" alt="image">
+                               </div>
+                               <div class="notify-text">
+                                   <p>Aglae Mayer</p>
+                                   <span class="msg">Hey I am waiting for you...</span>
+                                   <span>3:15 PM</span>
+                               </div>
+                           </a>
+                       </div>
+                   </div>
+               </li>
+               <li>
+                   <i class="ti-settings"></i>
+               </li>
+               <li class="settings-btn">
+                   <i class="ti-power-off"></i>
+               </li>
+           </ul>
+       </div>
+   </div>
+</div>
+<!-- header area end -->
---
+++ resources/views/templates/main.blade.php
@@ -0,0 +1,52 @@
+
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Futsal - Backend</title>
+    
+    @include('includes/style')
+    @stack('after-style')
+
+</head>
+
+<body>
+    <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <!-- preloader area start -->
+    <div id="preloader">
+        <div class="loader"></div>
+    </div>
+    <!-- preloader area end -->
+    <!-- page container area start -->
+    <div class="page-container">
+      @include('includes/sidebar')
+        <!-- main content area start -->
+        <div class="main-content">
+         @include('includes/header')
+         <div class="info" data-flashdata="{{session('info')}}"></div>
+         <div class="gagal" data-flashdata="{{session('gagal')}}"></div>
+         <div class="sukses" data-flashdata="{{session('sukses')}}"></div>
+         <div class="warning" data-flashdata="{{session('warning')}}"></div>
+
+         @yield('content')
+        </div>
+        <!-- main content area end -->
+        <!-- footer area start-->
+        <footer>
+            <div class="footer-area">
+                <p>© Copyright 2018. All right reserved. Template by <a href="https://colorlib.com/wp/">Colorlib</a>.</p>
+            </div>
+        </footer>
+        <!-- footer area end-->
+    </div>
+    <!-- page container area end -->
+    @stack('before-script')
+    @include('includes/script')
+    @stack('after-script')
+</body>
+
+</html>
